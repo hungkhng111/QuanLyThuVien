6481a725d8ddcbf77e0e431f9f6406455bffda13
hungkhng111 <101882096+hungkhng111@users.noreply.github.com>
2024-05-25 00:53:31 +0700

    Add project files.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        static bool _isSqlTypesLoaded = false;
+
+        public Default()
+        {
+            if (!_isSqlTypesLoaded)
+            {
+                SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~"));
+                _isSqlTypesLoaded = true;
+            }
+            return;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/ketnoi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public class ketnoi
+    {
+        public SqlConnection con;
+
+        public ketnoi()
+        {
+            //con = new SqlConnection("Data Source = SQL5075.site4now.net; Initial Catalog = db_aa304f_qltv; User Id = db_aa304f_qltv_admin; Password = matkhau");
+            //con = new SqlConnection("Data Source=LAPTOP-O1I8A0Q6\\SQLEXPRESS;Initial Catalog=QLTV_ASP;Integrated Security=True");
+            con = new SqlConnection("Data Source=hungsystem\\MSSQLSERVER02;Initial Catalog=QLTV;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+
+        }
+
+        [Obsolete]
+        public string mahoa(string caigi)
+        {
+            return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(caigi.Trim(), "SHA1");
+        }
+    }
+}
